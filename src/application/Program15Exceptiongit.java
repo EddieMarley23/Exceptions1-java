9b571d17abcad8ae6d6eb60cd639cdd710eee61c
Eddie Marley <marley_eddie@hotmail.com>
2024-04-29 19:49:09 -0300

    Solução ruim

--- src/application/Program15Exceptiongit.java
+++ src/application/Program15Exceptiongit.java
@@ -35,21 +35,15 @@ public class Program15Exceptiongit {
 			System.out.print("Check-out date (dd/MM/yyy): ");
 			checkOut = sdf.parse(sc.next());
 
-			Date now = new Date();
-			System.out.println(now);
-			if (checkIn.before(now) || checkOut.before(now)) {
-				System.out.println("Error in reservation: Reservation dates for update must be future dates");
-			} 
-			else if (!checkOut.after(checkIn)) {
-				System.out.print("Error in reservation: Check-out date must be after check-in date");
-			} 
-			else {
-				reservation.updateDates(checkIn, checkOut);
+		String error = reservation.updateDates(checkIn, checkOut);
+		if(error!= null) {
+			System.out.println("Error in reservation: "+ error);
+		}else {
 				System.out.println("Reservation: " + reservation);
-			}
+		}
 		}
 
 		sc.close();
 	}
-
 }
+
